64b7441abc3e48f87bd6d5f9d5bd39925a1d8f44
lou-ctlt <57705820+lou-ctlt@users.noreply.github.com>
2020-01-13 12:41:28 +0100

    initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            "firstname" => "Admin",
+            "lastname" => "Admin",
+            "email" => "adminemail@gmail.com",
+            "password" => Hash::make("00000000"),
+            "address" => "1 rue de l'adresse 33000 Bordeaux",
+            "license_plate" => "AA-000-AA",
+            "ID_number" => "012345678910"
+        ]);
+    }
+}
