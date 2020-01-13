9e36841a6c47c61e95a426ad25bb2ac33ef1c7df
Javier-Onate <javieronate@gmx.com>
2020-01-13 17:09:29 +0100

    mise en page du Home, information du User connecté affiché dans une card, un dossier Models a été créé avec un model MetaUser

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MetaUser;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $meta_user = DB::table('meta_users')->select("car", "electric_terminal")->where("users_id", Auth::user()->id)->get();
+        // $meta_users = new MetaUser();
+        // $meta_user = $meta_users->where("users_id", Auth::user()->id)->groupBy("items")->get();
+        // dd($meta_user);
+        return view('home')->with("meta_user", $meta_user);
+        //
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -18,6 +18,35 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+
+    <!-- Affichage des données personnel si la personne est connecté : START -->
+    @if (Auth::check())
+        <div class="row">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Bienvenu {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}, vous êtes correctement connecté !</h4>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Vos informations :</h5>
+                  <p class="card-text">Prénom : {{ Auth::user()->firstname }}</p>
+                  <p class="card-text">Nom : {{ Auth::user()->lastname }}</p>
+                  <p class="card-text">Email : {{ Auth::user()->email }}</p>
+                  <p class="card-text">Adresse : {{ Auth::user()->address }}</p>
+                  <p class="card-text">Plaque d'immatriculation : {{ Auth::user()->license_plate }}</p>
+                  <p class="card-text">CNI : {{ Auth::user()->ID_number }}</p>
+                  <p class="card-text">Voiture : {{ $meta_user[0]->car }}</p>
+                  <p class="card-text">Borne éléctrique : {{ $meta_user[0]->electric_terminal }}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-muted">
+                  Votre compte date du : {{ Auth::user()->created_at }}
+                </div>
+              </div>
+        </div>
+
+        {{-- {{ dd(Auth::user()) }} --}}
+    @endif
+    <!-- Affichage des données personnel si la personne est connecté : START -->
 </div>
 @endsection
